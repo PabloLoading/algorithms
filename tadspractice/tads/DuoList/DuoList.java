6775124d22970700d2d9aa9e586e12fcc288d58b
Joaquin Rodriguez <joaquinrodriguezcaussi@gmail.com>
2022-10-07 11:29:27 -0300

    Introduction of the DuoList

--- tadspractice/tads/DuoList/DuoList.java
+++ tadspractice/tads/DuoList/DuoList.java
@@ -4,6 +4,6 @@ public interface DuoList<K,T>{
     public void addFirst(K key, T data);
     public void remove(K key);
     public int size();
-    public boolean contains();
+    public boolean contains(K key);
     public T get(K key);
 }
